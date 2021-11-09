2618a515adfbbb63b004ee052acfc27c8aac5181
Chris S <chrisasmall+github@gmail.com>
2021-11-09 22:01:57 +0000

    npm init gatsby 🐱‍👤

---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "abletonpush-tutorials",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
